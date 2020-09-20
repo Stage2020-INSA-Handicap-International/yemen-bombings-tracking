d241dc364a5b5ae948caf3472926074b088c353b
mbettinger <47004523+mbettinger@users.noreply.github.com>
2020-09-20 22:00:12 +0200

    Masks examples

--- matlab/untitled2.m
+++ matlab/untitled2.m
@@ -7,12 +7,12 @@ outR=out(:,:,1);
 outB=out(:,:,2);
 
 %R0.25 B0.5 ou R&B0.2
-%outR=imbinarize(outR,0.20);
-%outB=imbinarize(outB,0.3);
+outR=imbinarize(outR,0.25);
+outB=imbinarize(outB,0.5);
 
 %outR=medfilt2(outR,[25 25]);
 %outB=medfilt2(outB,[25 25]);
-%out=double(cat(3,outR,outB,outB));
+out=double(cat(3,outR,outB,outB));
 %out=imgaussfilt(out,5);
 figure
 imshow(out)
